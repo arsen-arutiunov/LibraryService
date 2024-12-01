8850b1336b9e12efabb5938fb74d6f1e2e67bcd1
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-01 18:58:12 +0200

    Added books and swagger endpoints

--- LibraryService/urls.py
+++ LibraryService/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/books/", include("book.urls", namespace="book")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
