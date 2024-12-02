5c0cae9bddab0cb68be9c00bd7172472a3e0b183
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 13:10:15 +0200

    Add user endpoint to main urls.py

--- LibraryService/urls.py
+++ LibraryService/urls.py
@@ -22,6 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("book.urls", namespace="book")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
