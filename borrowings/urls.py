2243ddae6f594ed7af62e7b50cbd190f4cc64a9e
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 17:06:05 +0200

    Create urls and endpoint for borrowing app

---
+++ borrowings/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register(r"borrowings", BorrowingViewSet, basename="borrowing")
+
+urlpatterns = router.urls
+
+app_name = "borrowings"
