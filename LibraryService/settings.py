3bc90c0c1b101ac7331320344ef1e9f7889f568a
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 13:32:32 +0200

    Change header to Authorize

--- LibraryService/settings.py
+++ LibraryService/settings.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Authorize",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60 * 60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
