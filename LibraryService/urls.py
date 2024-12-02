a7256f2e6a09cfab6dfe863fb7755c0efb34d47b
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 13:39:20 +0200

    Renamed user endpoint to users

--- LibraryService/urls.py
+++ LibraryService/urls.py
@@ -22,7 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("book.urls", namespace="book")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/users/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
