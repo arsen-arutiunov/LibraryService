f8fb4755adbb938f252b78334cc12a705a37ed39
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 16:49:36 +0200

    Create borrowing model with save (borrowing) and return book functions

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,40 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book,
+                             on_delete=models.CASCADE,
+                             related_name="borrowings")
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name="borrowings")
+
+    def save(self, *args, **kwargs):
+        """Borrowing"""
+        if not self.pk:
+            self.book.inventory -= 1
+            self.book.save()
+        super().save(*args, **kwargs)
+
+    def return_book(self):
+        if not self.actual_return_date:
+            self.actual_return_date = date.today()
+            self.book.inventory += 1
+            self.book.save()
+            self.save()
+
+    def is_active(self):
+        return not self.actual_return_date
+
+    is_active.boolean = True
+
+    def __str__(self):
+        return f"{self.user} borrowed {self.book}"
