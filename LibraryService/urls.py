b7eafa793aab6e9c5018316f69fbceee245dfcdb
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 18:27:02 +0200

    Use black formater

--- LibraryService/urls.py
+++ LibraryService/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
-    path("api/", include("borrowings.urls",
-                                    namespace="borrowings")),
+    path("api/", include("borrowings.urls", namespace="borrowings")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
