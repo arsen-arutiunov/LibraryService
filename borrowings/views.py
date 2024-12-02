c71eb127c766fc3641205dea51183248d9ead38c
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-02 17:03:27 +0200

    Create view set for borrowing

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingCreateSerializer
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related("book", "user").all()
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    @action(detail=True, methods=["post"])
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+        if borrowing.actual_return_date:
+            return Response(
+                {"error": "This borrowing has already been returned."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        borrowing.return_book()
+        return Response(
+            {"detail": "Borrowing successfully returned."},
+            status=status.HTTP_200_OK
+        )
