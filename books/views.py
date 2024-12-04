03178fc5610ed51ee7982dc9d03c1c00f6364497
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-04 12:06:25 +0200

    Change permission if simple user

--- books/views.py
+++ books/views.py
@@ -44,4 +44,4 @@ class BookViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ("create", "update", "partial_update", "destroy"):
             return [permissions.IsAdminUser()]
-        return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
