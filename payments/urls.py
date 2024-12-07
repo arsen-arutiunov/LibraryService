bcf67a7a1cca05c3b8999b737ca4f7029aed9d62
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-12-07 15:23:23 +0200

    Update endpoints

--- payments/urls.py
+++ payments/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from .views import (
     PaymentListView,
     PaymentDetailView,
-    success_view,
-    cancel_view
+    PaymentSuccessView,
+    PaymentCancelView,
 )
 
 urlpatterns = [
     path("", PaymentListView.as_view(), name="payment-list"),
     path("<int:pk>/", PaymentDetailView.as_view(), name="payment-detail"),
-    path("success/", success_view, name="payment-success"),
-    path("cancel/", cancel_view, name="payment-cancel"),
+    path("success/", PaymentSuccessView.as_view(), name="payment-success"),
+    path("cancel/", PaymentCancelView.as_view(), name="payment-cancel"),
 ]
 
 app_name = "payments"
